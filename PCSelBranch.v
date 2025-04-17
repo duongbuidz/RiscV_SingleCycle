e3badf92549d57f530830518a57165625c864afe
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2025-04-18 02:11:20 +0700

    Update PCSelBranch.v

--- PCSelBranch.v
+++ PCSelBranch.v
@@ -1,7 +1,7 @@
 module NextPCSel(
     input [31:0] PC_plus4,
     input [31:0] PC_branch,       //  PC + immediate
-    input PCSel,                  
+    input branch_taken,                  
     output [31:0] next_PC
 );
     assign next_PC = (PCSel == 1'b0) ? PC_plus4 : PC_branch;
